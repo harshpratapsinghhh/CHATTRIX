b58b117ffe514b4cc09f9e316ec9946c865e44e3
harshpratapsinghhh <harshsuryavanshi169@gmail.com>
2025-12-28 14:43:20 +0530

    Message.java file added

---
+++ storage/Message.java
@@ -0,0 +1,26 @@
+package storage;
+
+public class Message {
+
+    private String sender;
+    private String content;
+    private long timestamp;
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
